99eabd1d2207f6acddb825685b49e993324d41ab
shri <85093480+shrikrushana9960@users.noreply.github.com>
2021-08-04 11:56:08 +0530

    Updates

---
+++ precache-manifest.4ded522ffaebe7820311f7ad4ccf4914.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a128ac53d0f698735b9cbce64281ac",
+    "url": "/learning-backend/index.html"
+  },
+  {
+    "revision": "11550fa33079325d5c8b",
+    "url": "/learning-backend/static/css/main.2df620c1.chunk.css"
+  },
+  {
+    "revision": "305a7ebe2a959844dada",
+    "url": "/learning-backend/static/js/2.e3267951.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/learning-backend/static/js/2.e3267951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11550fa33079325d5c8b",
+    "url": "/learning-backend/static/js/main.02df7585.chunk.js"
+  },
+  {
+    "revision": "dae9bbee29155b516e4b",
+    "url": "/learning-backend/static/js/runtime-main.82cd2fd0.js"
+  }
+]);
